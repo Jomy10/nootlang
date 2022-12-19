b1ffbc3457e9cea2d25de8eefe10210b5e7d390c
Jomy10 <jonas.vbs4@gmail.com>
2022-12-19 18:47:56 +0100

    new iterator implementation

--- parser/iterator.go
+++ parser/iterator.go
@@ -1,22 +1,31 @@
 package parser
 
+// A generic iterator over elements of type `T`
+type Iterator[T any] interface {
+	// Get the next element of the iterator. Returns false as the second argument
+	// if there are no more elements left in the Iterator
+	next() (*T, bool)
+	// Returns true if there is another element in the iterator
+	hasNext() bool
+	// Returns the next element of the iterator without advancing it. Returns false
+	// as the second argument if there are no more elements in the iterator
+	peek() (*T, bool)
+}
+
 // A simple iterator over an array of values T
-type Iterator[T any] struct {
+type ArrayIterator[T any] struct {
 	arr []T
-	// Points to the next item in the iterator
+	// points to the next item in the iterator
 	ptr int
 }
 
-func newIterator[T any](arr []T) Iterator[T] {
-	return Iterator[T]{
-		arr,
-		0,
-	}
+func newArrayIterator[T any](arr []T) ArrayIterator[T] {
+	return ArrayIterator[T]{arr, 0}
 }
 
 // Returns the next elements of the iterator and true, or false if there are no
 // more elements in the iterator
-func (iter *Iterator[T]) next() (*T, bool) {
+func (iter *ArrayIterator[T]) next() (*T, bool) {
 	if !iter.hasNext() {
 		return nil, false
 	}
@@ -26,15 +35,45 @@ func (iter *Iterator[T]) next() (*T, bool) {
 	return &val, true
 }
 
-// Returns wheter thee iterator has more elements
-func (iter *Iterator[T]) hasNext() bool {
+// Returns wheter the iterator has more elements
+func (iter *ArrayIterator[T]) hasNext() bool {
 	return len(iter.arr) != iter.ptr
 }
 
 // Peek at the next eleemnt of the iterator, without consuming it
-func (iter *Iterator[T]) peek() *T {
+func (iter *ArrayIterator[T]) peek() (*T, bool) {
 	if !iter.hasNext() {
-		return nil
+		return nil, false
 	}
-	return &iter.arr[iter.ptr]
+	return &iter.arr[iter.ptr], true
+}
+
+type ArrayOfPointersIterator[T any] struct {
+	arr []*T
+	ptr int
+}
+
+func newArrayOfPointerIterator[T any](arr []*T) ArrayOfPointersIterator[T] {
+	return ArrayOfPointersIterator[T]{arr, 0}
+}
+
+func (iter *ArrayOfPointersIterator[T]) next() (*T, bool) {
+	if !iter.hasNext() {
+		return nil, false
+	}
+
+	val := iter.arr[iter.ptr]
+	iter.ptr++
+	return val, true
+}
+
+func (iter *ArrayOfPointersIterator[T]) hasNext() bool {
+	return len(iter.arr) != iter.ptr
+}
+
+func (iter *ArrayOfPointersIterator[T]) peek() (*T, bool) {
+	if !iter.hasNext() {
+		return nil, false
+	}
+	return iter.arr[iter.ptr], true
 }
